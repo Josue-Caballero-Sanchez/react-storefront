41815a7cec9c4e2ea7633383ec48a13cc1e61033
Josue-Caballero-Sanchez <sanchez684.jc@gmail.com>
2026-03-09 16:35:24 -0700

    Made simple backend and made all items appear in homepage

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,48 @@
 import express, { Request, Response } from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (origin === process.env.ALLOWED_ORIGIN) {
+      callback(null, true);
+    }
+    else {
+      callback(new Error(`CORS blocked: ${origin} is not allowed`));
+    }
+  },
+  credentials: true,
+}));
+
+app.get('/', (req: Request, res: Response): void => {
   res.send("Hello, world from server!");
 });
+
+app.get('/products', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+
+    if(!response.ok){
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    res.send(data.products);
+  }
+  catch (error) {
+    console.error("Error fetching products: ", error);
+    res.status(500).send("Failed to fetch products");
+  }
+})
 
 app.listen(PORT, (): void => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- src/components/homeItems/HomeItems.tsx
+++ src/components/homeItems/HomeItems.tsx
@@ -1,148 +1,74 @@
 import styles from "./HomeItems.module.css";
 import { IoCartOutline } from "react-icons/io5";
 import { CiHeart } from "react-icons/ci";
+import { useEffect, useState } from "react";
+
+const BASE_API_URL = import.meta.env.VITE_API_URL;
 
 function HomeItems() {
+  const [products, setProducts] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchAllProducts(): Promise<void> {
+      try {
+        const response = await fetch(`${BASE_API_URL}/products`);
+
+        if (!response.ok) {
+          console.error(`HTTP error! status: ${response.status}`);
+          return;
+        }
+
+        const data = await response.json();
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products: ", error);
+      }
+    }
+
+    fetchAllProducts();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <div className={styles.item}>
-        <img
-          src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-          alt="item-picture"
-        />
-        <div className={styles.item__details}>
-          <p className={styles.category__text}>BEAUTY</p>
-          <p>Essence Mascara Lash Princess</p>
-          <p className={styles.rating__text}>2.56/5 </p>
-          <p className={styles.shipping__text}>Ships in 3-5 business days</p>
-          <div className={styles.price__container}>
-            <p>$9.99</p>
-            <p className={styles.original__price}>
-              ${(9.99 * 0.75).toFixed(2)}
+      {products.map((product) => (
+        <div key={product.id} className={styles.item}>
+          <img src={product.images[0]} alt={`${product.title} thumbnail`} />
+          <div className={styles.item__details}>
+            <p className={styles.category__text}>
+              {product.category.toUpperCase()}
             </p>
-            <p className={styles.discount__text}>-25%</p>
-          </div>
-          <button>
-            <IoCartOutline />
-            Add to Cart
-          </button>
-          <button className={styles.favorite__button}>
-            <CiHeart />
-            Favorite
-          </button>
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <img
-          src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-          alt="item-picture"
-        />
-        <div className={styles.item__details}>
-          <p className={styles.category__text}>BEAUTY</p>
-          <p>Essence Mascara Lash Princess</p>
-          <p className={styles.rating__text}>2.56/5 </p>
-          <p className={styles.shipping__text}>Ships in 3-5 business days</p>
-          <div className={styles.price__container}>
-            <p>$9.99</p>
-            <p className={styles.original__price}>
-              ${(9.99 * 0.75).toFixed(2)}
+            <p>{product.title}</p>
+            <p className={styles.rating__text}>{product.rating}/5</p>
+            <p className={styles.shipping__text}>
+              {product.shippingInformation}
             </p>
-            <p className={styles.discount__text}>-25%</p>
+            <div className={styles.price__container}>
+              <p>${product.price}</p>
+              <p className={styles.original__price}>
+                $
+                {(
+                  product.price *
+                  (product.discountPercentage / 100 + 1)
+                ).toFixed(2)}
+              </p>
+              <p className={styles.discount__text}>
+                -{product.discountPercentage.toFixed(2)}%
+              </p>
+            </div>
+            <div>
+              <button>
+                <IoCartOutline />
+                Add to Cart
+              </button>
+              <button className={styles.favorite__button}>
+                <CiHeart />
+                Favorite
+              </button>
+            </div>
           </div>
-          <button>
-            <IoCartOutline />
-            Add to Cart
-          </button>
-          <button className={styles.favorite__button}>
-            <CiHeart />
-            Favorite
-          </button>
         </div>
-      </div>
-      <div className={styles.item}>
-        <img
-          src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-          alt="item-picture"
-        />
-        <div className={styles.item__details}>
-          <p className={styles.category__text}>BEAUTY</p>
-          <p>Essence Mascara Lash Princess</p>
-          <p className={styles.rating__text}>2.56/5 </p>
-          <p className={styles.shipping__text}>Ships in 3-5 business days</p>
-          <div className={styles.price__container}>
-            <p>$9.99</p>
-            <p className={styles.original__price}>
-              ${(9.99 * 0.75).toFixed(2)}
-            </p>
-            <p className={styles.discount__text}>-25%</p>
-          </div>
-          <button>
-            <IoCartOutline />
-            Add to Cart
-          </button>
-          <button className={styles.favorite__button}>
-            <CiHeart />
-            Favorite
-          </button>
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <img
-          src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-          alt="item-picture"
-        />
-        <div className={styles.item__details}>
-          <p className={styles.category__text}>BEAUTY</p>
-          <p>Essence Mascara Lash Princess</p>
-          <p className={styles.rating__text}>2.56/5 </p>
-          <p className={styles.shipping__text}>Ships in 3-5 business days</p>
-          <div className={styles.price__container}>
-            <p>$9.99</p>
-            <p className={styles.original__price}>
-              ${(9.99 * 0.75).toFixed(2)}
-            </p>
-            <p className={styles.discount__text}>-25%</p>
-          </div>
-          <button>
-            <IoCartOutline />
-            Add to Cart
-          </button>
-          <button className={styles.favorite__button}>
-            <CiHeart />
-            Favorite
-          </button>
-        </div>
-      </div>
-
-      <div className={styles.item}>
-        <img
-          src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-          alt="item-picture"
-        />
-        <div className={styles.item__details}>
-          <p className={styles.category__text}>BEAUTY</p>
-          <p>Essence Mascara Lash Princess</p>
-          <p className={styles.rating__text}>2.56/5 </p>
-          <p className={styles.shipping__text}>Ships in 3-5 business days</p>
-          <div className={styles.price__container}>
-            <p>$9.99</p>
-            <p className={styles.original__price}>
-              ${(9.99 * 0.75).toFixed(2)}
-            </p>
-            <p className={styles.discount__text}>-25%</p>
-          </div>
-          <button>
-            <IoCartOutline />
-            Add to Cart
-          </button>
-          <button className={styles.favorite__button}>
-            <CiHeart />
-            Favorite
-          </button>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
